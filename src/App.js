f46b95650d253d93ff72f341730132edc1372c24
Ashutosh Meena <ashumeena2802@gmail.com>
2021-04-07 00:14:49 +0530

    Completed lesson 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
+const title = 'Welcome to the new Blog'
+const likes = 50;
+const count = [10,20,30]
+const link = "http://www.google.com"
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+      <div className="content">
+      <h1>{title}</h1>
+      <Home/>
+      <p>Liked {likes} times</p>
+      <p>{Math.round(23.4) * 10}</p>
+      <a href={link}>Google Site</a>
+
+      </div>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div className="home">
+        <h2>Homepage</h2>
+        <button>Click</button>
+            
+        </div>
+    )
+}
